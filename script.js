9dcfb7b5aebcf330a87049526224a10d16d5c607
Pietro Nencini <nencinipietro2006@gmail.com>
2024-10-29 10:47:28 +0100

    almost completed

---
+++ script.js
@@ -0,0 +1,29 @@
+
+let usa_flag = "./images/us-flag.gif";
+let jpn_flag = "./images/ja-flag.gif";
+let uk_flag = "./images/uk-flag.gif";
+let swi_flag = "./images/sz-flag.gif";
+
+
+function updateCountryFlag() {
+    let image = document.getElementById("current_country");
+    let selected_country = document.getElementById("foreign_country").value;
+    let img_path = "";
+    switch(selected_country) {
+        case "us_d":
+            img_path = usa_flag;
+            break;
+        case "sz_f":
+            img_path = swi_flag;
+            break;
+        case "jp_y":
+            img_path = jpn_flag;
+            break;
+        case "uk_p":
+            img_path = uk_flag;
+            break;
+        default:
+            console.log("ERORRE");
+    }
+    image.src = img_path;
+}
